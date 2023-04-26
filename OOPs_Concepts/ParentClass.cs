c1a91d9681b1b68c5260e034044373d97c74c2b3
Pranav Waghmare <piyushw55@gmail.com>
2023-04-26 20:27:16 +0530

    [Pranav]Added: Ability to perform inheritance operation

---
+++ OOPs_Concepts/ParentClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_Concepts
+{
+    public class BaseClass
+    {
+        public string name;
+        public void BaseClassMethod()
+        {
+            Console.WriteLine("This is a base class");
+        }
+
+        public virtual void BaseClassVirtualMethod()
+        {
+            Console.WriteLine("This is my method for parent class");
+        }
+    }
+
+    public class ChildClass : BaseClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("This is my Parent class");
+
+        }
+        public void ChildClassMethod()
+        {
+            Console.WriteLine("This is my method for parent class");
+        }
+
+        public override void BaseClassVirtualMethod()
+        {
+            Console.WriteLine("This is a Child class overrided");
+        }
+
+
+
+    }
+
+    
+}
--- OOPs_Concepts/Program.cs
+++ OOPs_Concepts/Program.cs
@@ -4,22 +4,40 @@
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("\nClasses and Methods  Example");
+
             ClassName className= new ClassName();
             className.printName();
             className.printType("Pranav");
-            
 
+
+
+            Console.WriteLine("\nConstructor Example");
             //default constructor
             Constructor ctor = new Constructor();
             //Parameteriezed constructor
             Constructor pctor = new Constructor("Pranav",25,'M');
 
-
+            Console.WriteLine("\nInterface  Example");
             Interface Myinterface = new Interface();
             Myinterface.WithoutImplementationMethod();
 
+            Console.WriteLine("\n Enums Example");
             Console.WriteLine("EnumTypes : " + Project.FIFTH);
             Console.WriteLine("EnumTypes : " + (int)Project.FIFTH);
+
+            Console.WriteLine("\n Inheritance Example");
+            //ChildClass parentClass= new ChildClass();
+
+            //parentClass.ChildClassMethod();
+
+            // parentClass.BaseClassMethod();
+
+            //parentClass.BaseClassMethod();
+
+            BaseClass parentClass= new BaseClass();
+            parentClass.BaseClassVirtualMethod();
             Console.ReadKey();
 
         }
